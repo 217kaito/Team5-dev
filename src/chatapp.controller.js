2e812ecf44d03dc81fbf3f0ab2d64da62f3e9ef4
skamys <ee217101@meiji.ac.jp>
2023-11-26 01:08:35 +0900

    #2 fix controller

--- src/chatapp.controller.js
+++ src/chatapp.controller.js
@@ -1,12 +1,9 @@
 "use strict";
 const {
-  createMessage,
-  markMessageAsReplied,
-  unmarkMessageAsReplied,
-  generateMessageId,
+  createPost,
   createThread,
   getThreads,
-  getMessagesInThread,
+  getPostsByThreadId,
 } = require("./chatapp.service");
 
 const PromiseRouter = require("express-promise-router");
@@ -15,55 +12,33 @@ const router = PromiseRouter();
 
 // HTTP POSTメソッドに対応したエンドポイントの処理を定義
 // フォームからの入力を用いて新しいメッセージを作成するエンドポイント
-// 入力が増える可能性もあることから、オブジェクトを利用
-router.post("/threads/:threadsId/add", async (req, res) => {
-  const message = {
-    id: generateMessageId(),
-    name: req.body.name,
-    title: req.body.title,
-    content: req.body.message,
-    timestamp: timestamp,
-  };
-
-  await createMessage(message);
-  res.redirect("/");
-});
-
-// 新しいメッセージの投稿画面を表示するエンドポイント
-router.get("/add", (req, res) => {
-  res.render("add");
-});
-
-// メッセージを返信済みにするエンドポイント
-router.get("/replied/:id", async (req, res) => {
-  await markMessageAsReplied(parseInt(req.params.id, 10));
-  res.redirect("/");
-});
-
-// メッセージを未返信にするエンドポイント
-router.get("/unreplied/:id", async (req, res) => {
-  await unmarkMessageAsReplied(parseInt(req.params.id, 10));
-  res.redirect("/");
+router.post("/thread/:threadId/post", async (req, res) => {
+  const ip = req.ip;
+  const threadId = req.params.threadId;
+  const content = req.body.content;
+  await createPost(ip, threadId, content);
+  res.redirect(`/threads/${threadId}`);
 });
 
 // スレッドの作成エンドポイント
-router.post("/createThread", async (req, res) => {
-  const threadName = req.body.threadName;
-  await createThread(threadName);
+router.post("/threads/create", async (req, res) => {
+  const ip = req.ip;
+  const title = req.body.title;
+  await createThread(ip, title);
   res.redirect("/threads"); // スレッド一覧ページにリダイレクト
 });
 
 // スレッド一覧を表示するエンドポイント
 router.get("/threads", async (req, res) => {
   const threads = await getThreads();
-  res.render("threads", { threads });
+  res.render("threads-list", { threads });
 });
 
 // 特定のスレッド内のメッセージ一覧を表示するエンドポイント
 router.get("/threads/:threadId", async (req, res) => {
   const threadId = req.params.threadId;
-  const messages = await getMessagesInThread(threadId);
-  res.render("threadMessages", { messages, threadId });
+  const posts = await getPostsByThreadId(threadId);
+  res.render("thread-view", { posts });
 });
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 // src/index.js
 "use strict";
 
+const path = require("path");
+
 const express = require("express");
+const methodOverride = require("method-override");
 const app = express();
 const PORT = 3000;
+
+const { router } = require("./chatapp.controller");
 
 app.get("/", (req, res) => {
   res.send("Hello, team5!");
 });
+
+app.use(express.static(path.resolve(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "views"));
+app.use(router);
+
+app.use(methodOverride("_method"));
 
 app.listen(PORT, () => {
   console.log(`team5 app listening on port ${PORT}`);
